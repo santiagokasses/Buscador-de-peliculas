ec8478e9bb59a666314a030c6c69167c580272d9
CAMILA AYLEN LOPEZ <47344372@ort>
2023-04-17 10:40:43 -0300

    nose

--- main.js
+++ main.js
@@ -33,11 +33,16 @@ const buscarProducto = () => {
     fetch(`https://dummyjson.com/products/search?q=${productoAbuscarpornombre}`)
     .then(res => res.json())
     .then(res =>  {
-        valores = document.getElementById("productosencontrados");
-        producto = document.createElement("li")
-        producto.innerHTML = `Producto: ${res.title} - Descripción: ${res.description} - Precio: ${res.price}`
-        valores.appendChild(producto)}
-        )
+        valores = document.getElementById("productosencontrados").innerHTML += `
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${json().images}" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${res.producto.title}</h5>
+        <p class="card-text"${res.description}</p>
+        <h6 class="card-title">${res.price}</h6>
+        </div>
+      </div>`
+        })
     .catch(err => console.error(err))
 }
 /*const buscarProducto = () => {
