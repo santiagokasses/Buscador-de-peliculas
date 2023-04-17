f5faba85621cf82426a8bd4f1a1234d08b3b7c9b
CAMILA AYLEN LOPEZ <47344372@ort>
2023-04-17 08:56:18 -0300

    js

--- main.js
+++ main.js
@@ -1,50 +1,17 @@
-var link = "https://www.omdbapi.com/?apikey=8f8de035&";
-
-var buscarporesto;
-
-function buscarpor (){
-    var nombrePelicula = document.getElementById("porTitulo")
-    var añoPelicula = document.getElementById("porAño")
-    var directorPelicula = document.getElementById("porDirector")
-    buscarporesto = "s=" + nombrePelicula
-}
-
-link += buscarporesto;
+const productoAbuscar = document.getElementById(nombredelproducto);
 
 console.log("Hago consulta - fetch")
-fetch(link)
+fetch("https://dummyjson.com/products")
     .then(res => res.json())
     .then(res => {
         console.log("obtuve respuesta")
         valores = document.getElementById("peliculasEncontradas");
-        forEach(actual => {
-            pelicula = document.createElement("li")
-            pelicula.innerHTML = `Titulo: ${actual.Title} - fecha: ${actual.Released} - director: ${actual.Director}`
-            valores.appendChild(pelicula) 
+        res.data.forEach(actual => {
+            if (actual.title == productoAbuscar) {
+                pelicula = document.createElement("li")
+                pelicula.innerHTML = `Producto: ${actual.title} - Descripción: ${actual.description} - Precio: ${actual.price}`
+                valores.appendChild(pelicula)
+            }
         });
     })
 console.log("Fin consulta - fetch")
-
-/*console.log("Hago consulta - axios");
-(async () => {
-    console.log("Hago async consulta - axios")
-    await axios({
-        method: 'get',
-        url: 'https://www.omdbapi.com/?i=tt3896198&apikey=8f8de035',
-    })
-        .then(res => {
-            console.log("res", res)
-            valores = document.getElementById("peliculasEncontradas");
-            res.data.forEach(actual => {
-
-                pelicula = document.createElement("li")
-                pelicula.innerHTML = `Nombre: ${actual.nombre} - compra: ${actual.compra} - venta: ${actual.venta}`
-                valores.appendChild(pelicula)
-
-                console.log(res)
-            })
-        })
-        .catch(err => console.error("error", err))
-    console.log("Fin async consulta - axios")
-})();
-console.log("Fin consulta - axios")*/
