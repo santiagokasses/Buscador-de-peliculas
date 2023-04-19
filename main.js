d0b776b7b93f39cb4e1c45e9e80cf736094a5cfe
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-04-19 09:05:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -36,11 +36,11 @@ const buscarProducto = () => {
         res.products.forEach(i => {
             valores = document.getElementById("productosencontrados").innerHTML += `
             <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${i.images[0]}" alt="Card image cap">
+            <img class="card-img-top" src="${i.thumbnail}" alt="Card image cap">
             <div class="card-body">
             <h5 class="card-title">${i.title}</h5>
-            <p class="card-text"${i.description}</p>
-            <h6 class="card-title">${i.price}</h6>
+            <p class="card-text"> ${i.description}</p>
+            <h6 class="card-title">$${i.price}</h6>
             </div>
             </div>`
         })
