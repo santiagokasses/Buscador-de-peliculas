e41e3e4b923c58c4217e78983953a54e8c382fdd
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-04-22 11:55:05 -0300

    funciona todo
    cambie precio por id porque no buscaba nada, arregle el dropdown y todas las busquedas funcionan

--- main.js
+++ main.js
@@ -1,36 +1,59 @@
+const input = document.getElementById("input")
 
+var idBuscar
+
+const buscar = id =>{ 
+    idBuscar = id
+    input.setAttribute('id',`${idBuscar}`)
+    document.getElementById("dropdown").innerHTML = `${idBuscar}`
+}
 
 const buscarProducto = () => {
-    const productoAbuscarpornombre = document.getElementById("porproducto").value;
-    const productoAbuscarporprecio = document.getElementById("porprecio").value;
-    const productoAbuscarporcategoria = document.getElementById("porcategoria").value;
     let link = ""
-    if(!productoAbuscarporprecio && !productoAbuscarporcategoria)
+    if(idBuscar == "Producto")
     {
+        var productoAbuscarpornombre = document.getElementById("Producto").value
         link = "https://dummyjson.com/products/search?q=" + productoAbuscarpornombre
+        console.log(link)
     }
-    else if(!productoAbuscarporprecio && productoAbuscarporcategoria && !productoAbuscarpornombre){
-        link = "https://dummyjson.com/products/categories/" + productoAbuscarporcategoria
+    else if(idBuscar == "ID"){
+        var productoAbuscarporID = document.getElementById("ID").value
+        link = "https://dummyjson.com/products/" + productoAbuscarporID
+        console.log(link)
     }
-    else if(productoAbuscarporprecio && !productoAbuscarporcategoria){
-        link = "https://dummyjson.com/products/price/" + productoAbuscarporprecio
+    else if(idBuscar == "Categoria"){
+        var productoAbuscarporcategoria = document.getElementById("Categoria").value
+        link = "https://dummyjson.com/products/category/" + productoAbuscarporcategoria
+        console.log(link)
     }
     fetch(link)
     .then(res => res.json())
     .then(res =>  { 
-        res.products.forEach(i => {
+        if(res.products) {
+            res.products.forEach(i => {
+                valores = document.getElementById("productosencontrados").innerHTML += `
+                <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="${i.thumbnail}" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">${i.title}</h5>
+                <p class="card-text"> ${i.description}</p>
+                <h6 class="card-title">$${i.price}</h6>
+                </div>
+                </div>`
+            })
+        }
+        else if(res.length = 1){
             valores = document.getElementById("productosencontrados").innerHTML += `
             <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${i.thumbnail}" alt="Card image cap">
+            <img class="card-img-top" src="${res.thumbnail}" alt="Card image cap">
             <div class="card-body">
-            <h5 class="card-title">${i.title}</h5>
-            <p class="card-text"> ${i.description}</p>
-            <h6 class="card-title">$${i.price}</h6>
+            <h5 class="card-title">${res.title}</h5>
+            <p class="card-text"> ${res.description}</p>
+            <h6 class="card-title">$${res.price}</h6>
             </div>
             </div>`
-        })
-        
-      console.log(res)
+        }
+        console.log(res)
         })
         
     .catch(err => console.error(err))
